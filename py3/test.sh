3faa2193aff175e0e45d6aa885210f32d1759324
root <root@fedora-21-dvm>
2016-11-23 21:56:40 +0000

    fix test scripts

--- py3/test.sh
+++ py3/test.sh
@@ -2,10 +2,15 @@
 
 set -x
 
-python3 -m venv $PWD/virtualenv
+python3 -m venv --without-pip $PWD/virtualenv
 
 source $PWD/virtualenv/bin/activate
 
-pip install -m test-reqs.txt
+curl https://bootstrap.pypa.io/get-pip.py | python
+deactivate
 
-nose2
+source $PWD/virtualenv/bin/activate
+
+pip install -r test-reqs.txt
+
+python3 -m nose
